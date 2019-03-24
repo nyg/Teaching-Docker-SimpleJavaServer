69c3fbef01458b04ad1af08dfebcfc385278eec6
nyg <nyg@users.noreply.github.com>
2019-03-24 13:15:57 +0100

    Added more commands in run-container.sh

--- docker/run-container.sh
+++ docker/run-container.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-docker run java-streaming-server
+# run command CMD (defined in Dockerfile) in a new container using image java-streaming-server
+# --rm   remove container when it exits
+# -p     link host port with container port
+# --name name of the container
+docker run --rm -p 2205:2205 --name jss java-streaming-server
+docker run --rm -p 2205:2205 --name jss -d java-streaming-server
+
+# run sh in a new container (to "explore" it)
+docker run -it java-streaming-server /bin/sh
+
+# execute sh into a running container
+docker exec -it <container_name> /bin/sh
+
+# attach stdin, stdout & stderr to a running container
+docker attach <container_name>
